51d8a3478bf429baabbb4694dbf7702c83205687
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-04-26 17:26:59 -0700

    complete lab

--- main.js
+++ main.js
@@ -3,7 +3,50 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded",function(){
+  let valueHeart = document.getElementsByClassName('like-glyph')
+  for (const heart of valueHeart) {
+    heart.addEventListener('click', buttonClick)
+  }
+})
 
+function buttonClick(e){
+  
+  const heart = e.target;
+  mimicServerCall()
+  .then((data) => {
+    setTimeout(function() {
+      sethiddenclass(true)  
+    
+    if(heart.classList.contains('activated-heart')===false){
+      heart.classList.add("activated-heart")
+    }
+    else {
+      heart.classList.remove("activated-heart")
+    }
+  }, 300)
+      
+    
+    
+
+  })
+  
+  .catch((error) => {
+    
+    let md= document.getElementById('modal')
+    md.textContent=error
+    sethiddenclass(false)
+  })
+}
+function sethiddenclass(value){
+  let hdValue= document.getElementById('modal')
+  if(value===false){
+    hdValue.classList.remove("hidden");
+  }
+  else if(value===true){
+    hdValue.classList.add("hidden");
+  }
+}
 
 
 
@@ -20,6 +63,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
+      
     }, 300);
   });
 }
